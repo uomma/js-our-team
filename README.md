5e019792651a10c38d53016472f10478af56f5de
uomma <marcelloleone975@gmail.com>
2023-03-07 17:07:55 +0100

    BONUS 1

--- README.md
+++ README.md
@@ -7,9 +7,13 @@ foto
 Creare l’array di oggetti con le informazioni fornite. -->
 <!-- MILESTONE 1:
 Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto -->
-MILESTONE 2:
+<!-- MILESTONE 2:
 Stampare le stesse informazioni su DOM sottoforma di stringhe
-
+ -->
+<!-- BONUS 1:
+Trasformare la stringa foto in una immagine effettiva -->
+BONUS 2:
+Organizzare i singoli membri in card/schede
 
 Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
 Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
--- main.js
+++ main.js
@@ -2,112 +2,134 @@
 const ourTeam = [
     {
         'nome': 'Wayne Barnett',
-        'ruolo':'Founder & CEO',
+        'ruolo': 'Founder & CEO',
         'foto': 'wayne-barnett-founder-ceo.jpg'
 
     },
 
-   {
+    {
         'nome': 'Angela Caroll',
-        'ruolo':'Chief Editor',
-        'foto':'angela-caroll-chief-editor.jpg'
+        'ruolo': 'Chief Editor',
+        'foto': 'angela-caroll-chief-editor.jpg'
 
     },
 
     {
         'nome': 'Walter Gordon',
-        'ruolo':'Office Manager',
-        'foto':'walter-gordon-office-manager.jpg'
+        'ruolo': 'Office Manager',
+        'foto': 'walter-gordon-office-manager.jpg'
 
     },
 
     {
         'nome': 'Angela Lopez',
-        'ruolo':'Social Media Manager',
-        'foto':'angela-lopez-social-media-manager.jpg'
+        'ruolo': 'Social Media Manager',
+        'foto': 'angela-lopez-social-media-manager.jpg'
 
     },
 
     {
         'nome': 'Scott Estrada',
-        'ruolo':'Developer',
-        'foto':'scott-estrada-developer.jpg'
+        'ruolo': 'Developer',
+        'foto': 'scott-estrada-developer.jpg'
 
     },
 
     {
         'nome': 'Barbara Ramos',
-        'ruolo':'Graphic Designer',
-        'foto':'barbara-ramos-graphic-designer.jpg'
+        'ruolo': 'Graphic Designer',
+        'foto': 'barbara-ramos-graphic-designer.jpg'
 
-    }, 
+    },
 ];
 
-
+//PERSONA 0
 console.log(ourTeam[0].nome)
 console.log(ourTeam[0].ruolo)
 console.log(ourTeam[0].foto)
 
 const personaNome0 = document.querySelector('.nome0');
 personaNome0.innerHTML = (ourTeam[0].nome);
+
 const personaRuolo0 = document.querySelector('.ruolo0');
 personaRuolo0.innerHTML = (ourTeam[0].ruolo);
-const personaFoto0 = document.querySelector('.foto0');
-personaFoto0.innerHTML = (ourTeam[0].foto);
 
+const img0 = document.createElement("img");
+img0.src = "IMG/wayne-barnett-founder-ceo.jpg";
+document.body.appendChild(img0);
+
+
+//PERSONA 1
 console.log(ourTeam[1].nome)
 console.log(ourTeam[1].ruolo)
 console.log(ourTeam[1].foto)
 
 const personaNome1 = document.querySelector('.nome1');
 personaNome1.innerHTML = (ourTeam[1].nome);
+
 const personaRuolo1 = document.querySelector('.ruolo1');
 personaRuolo1.innerHTML = (ourTeam[1].ruolo);
-const personaFoto1 = document.querySelector('.foto1');
-personaFoto1.innerHTML = (ourTeam[1].foto);
 
+const img1 = document.createElement("img");
+img1.src = "IMG/angela-caroll-chief-editor.jpg";
+document.body.appendChild(img1);
+
+//PERSONA 2
 console.log(ourTeam[2].nome)
 console.log(ourTeam[2].ruolo)
 console.log(ourTeam[2].foto)
 
 const personaNome2 = document.querySelector('.nome2');
 personaNome2.innerHTML = (ourTeam[2].nome);
+
 const personaRuolo2 = document.querySelector('.ruolo2');
 personaRuolo2.innerHTML = (ourTeam[2].ruolo);
-const personaFoto2 = document.querySelector('.foto2');
-personaFoto2.innerHTML = (ourTeam[2].foto);
 
+const img2 = document.createElement("img");
+img2.src = "IMG/walter-gordon-office-manager.jpg";
+document.body.appendChild(img2);
+
+//PERSONA 3
 console.log(ourTeam[3].nome)
 console.log(ourTeam[3].ruolo)
 console.log(ourTeam[3].foto)
 
 const personaNome3 = document.querySelector('.nome3');
 personaNome3.innerHTML = (ourTeam[3].nome);
+
 const personaRuolo3 = document.querySelector('.ruolo3');
 personaRuolo3.innerHTML = (ourTeam[3].ruolo);
-const personaFoto3 = document.querySelector('.foto3');
-personaFoto3.innerHTML = (ourTeam[3].foto);
 
+const img3 = document.createElement("img");
+img3.src = "IMG/angela-lopez-social-media-manager.jpg";
+document.body.appendChild(img3);
+
+//PERSONA 4
 console.log(ourTeam[4].nome)
 console.log(ourTeam[4].ruolo)
 console.log(ourTeam[4].foto)
 
 const personaNome4 = document.querySelector('.nome4');
 personaNome4.innerHTML = (ourTeam[4].nome);
+
 const personaRuolo4 = document.querySelector('.ruolo4');
 personaRuolo4.innerHTML = (ourTeam[4].ruolo);
-const personaFoto4 = document.querySelector('.foto4');
-personaFoto4.innerHTML = (ourTeam[4].foto);
 
+const img4 = document.createElement("img");
+img4.src = "IMG/scott-estrada-developer.jpg";
+document.body.appendChild(img4);
+
+//PERSONA 5
 console.log(ourTeam[5].nome)
 console.log(ourTeam[5].ruolo)
 console.log(ourTeam[5].foto)
 
 const personaNome5 = document.querySelector('.nome5');
 personaNome5.innerHTML = (ourTeam[5].nome);
+
 const personaRuolo5 = document.querySelector('.ruolo5');
 personaRuolo5.innerHTML = (ourTeam[5].ruolo);
-const personaFoto5 = document.querySelector('.foto5');
-personaFoto5.innerHTML = (ourTeam[5].foto);
 
-
+const img5 = document.createElement("img");
+img5.src = "IMG/barbara-ramos-graphic-designer.jpg";
+document.body.appendChild(img5); 
