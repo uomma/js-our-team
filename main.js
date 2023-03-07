6f36257d8efe07e1c4ac52f4e6d4ca1b17047b1c
uomma <marcelloleone975@gmail.com>
2023-03-07 16:36:01 +0100

    stampate info nel dom

--- main.js
+++ main.js
@@ -48,25 +48,66 @@ console.log(ourTeam[0].nome)
 console.log(ourTeam[0].ruolo)
 console.log(ourTeam[0].foto)
 
+const personaNome0 = document.querySelector('.nome0');
+personaNome0.innerHTML = (ourTeam[0].nome);
+const personaRuolo0 = document.querySelector('.ruolo0');
+personaRuolo0.innerHTML = (ourTeam[0].ruolo);
+const personaFoto0 = document.querySelector('.foto0');
+personaFoto0.innerHTML = (ourTeam[0].foto);
+
 console.log(ourTeam[1].nome)
 console.log(ourTeam[1].ruolo)
 console.log(ourTeam[1].foto)
 
+const personaNome1 = document.querySelector('.nome1');
+personaNome1.innerHTML = (ourTeam[1].nome);
+const personaRuolo1 = document.querySelector('.ruolo1');
+personaRuolo1.innerHTML = (ourTeam[1].ruolo);
+const personaFoto1 = document.querySelector('.foto1');
+personaFoto1.innerHTML = (ourTeam[1].foto);
 
 console.log(ourTeam[2].nome)
 console.log(ourTeam[2].ruolo)
 console.log(ourTeam[2].foto)
 
+const personaNome2 = document.querySelector('.nome2');
+personaNome2.innerHTML = (ourTeam[2].nome);
+const personaRuolo2 = document.querySelector('.ruolo2');
+personaRuolo2.innerHTML = (ourTeam[2].ruolo);
+const personaFoto2 = document.querySelector('.foto2');
+personaFoto2.innerHTML = (ourTeam[2].foto);
+
 console.log(ourTeam[3].nome)
 console.log(ourTeam[3].ruolo)
 console.log(ourTeam[3].foto)
+
+const personaNome3 = document.querySelector('.nome3');
+personaNome3.innerHTML = (ourTeam[3].nome);
+const personaRuolo3 = document.querySelector('.ruolo3');
+personaRuolo3.innerHTML = (ourTeam[3].ruolo);
+const personaFoto3 = document.querySelector('.foto3');
+personaFoto3.innerHTML = (ourTeam[3].foto);
 
 console.log(ourTeam[4].nome)
 console.log(ourTeam[4].ruolo)
 console.log(ourTeam[4].foto)
 
+const personaNome4 = document.querySelector('.nome4');
+personaNome4.innerHTML = (ourTeam[4].nome);
+const personaRuolo4 = document.querySelector('.ruolo4');
+personaRuolo4.innerHTML = (ourTeam[4].ruolo);
+const personaFoto4 = document.querySelector('.foto4');
+personaFoto4.innerHTML = (ourTeam[4].foto);
+
 console.log(ourTeam[5].nome)
 console.log(ourTeam[5].ruolo)
 console.log(ourTeam[5].foto)
+
+const personaNome5 = document.querySelector('.nome5');
+personaNome5.innerHTML = (ourTeam[5].nome);
+const personaRuolo5 = document.querySelector('.ruolo5');
+personaRuolo5.innerHTML = (ourTeam[5].ruolo);
+const personaFoto5 = document.querySelector('.foto5');
+personaFoto5.innerHTML = (ourTeam[5].foto);
 
 
