b3de59b880f5d030324792c84da863ce187b2770
uomma <marcelloleone975@gmail.com>
2023-03-07 16:00:55 +0100

    creato array di oggetti

--- main.js
+++ main.js
@@ -7,40 +7,40 @@ const ourTeam = [
 
     },
 
-    /* {
-        'nome':
-        'ruolo':
-        'foto':
+   {
+        'nome': 'Angela Caroll',
+        'ruolo':'Chief Editor',
+        'foto':'angela-caroll-chief-editor.jpg'
 
     },
 
     {
-        'nome':
-        'ruolo':
-        'foto':
+        'nome': 'Walter Gordon',
+        'ruolo':'Office Manager',
+        'foto':'walter-gordon-office-manager.jpg'
 
     },
 
     {
-        'nome':
-        'ruolo':
-        'foto':
+        'nome': 'Angela Lopez',
+        'ruolo':'Social Media Manager',
+        'foto':'angela-lopez-social-media-manager.jpg'
 
     },
 
     {
-        'nome':
-        'ruolo':
-        'foto':
+        'nome': 'Scott Estrada',
+        'ruolo':'Developer',
+        'foto':'scott-estrada-developer.jpg'
 
     },
 
     {
-        'nome':
-        'ruolo':
-        'foto':
+        'nome': 'Barbara Ramos',
+        'ruolo':'Graphic Designer',
+        'foto':'barbara-ramos-graphic-designer.jpg'
 
-    }, */
+    }, 
 ];
 
 
